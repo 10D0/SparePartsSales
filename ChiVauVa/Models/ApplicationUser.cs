6c8d63e32fe1465389b5681ecb1c07d4d70c41a3
Kiselev Stepan <step-2911@mail.ru>
2025-03-13 20:45:10 +0300

    лютый хеш пароля в aspnetusers

--- ChiVauVa/Models/ApplicationUser.cs
+++ ChiVauVa/Models/ApplicationUser.cs
@@ -6,6 +6,8 @@ namespace ChiVauVa.Models
     public class ApplicationUser : IdentityUser
     {
         public int LegacyUserId { get; set; }
+        //public string Phone { get; set; }
+        //public string FullName { get; set; }
     }
 
 }
--- ChiVauVa/Program.cs
+++ ChiVauVa/Program.cs
@@ -1,7 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
 using ChiVauVa.Data;
 using ChiVauVa.Models;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,12 +13,12 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-//builder.Services.AddDefaultIdentity<Users>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.LoginPath = "/Identity/Account/Login";
     options.AccessDeniedPath = "/Identity/Account/AccessDenied";
 });
+
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
@@ -26,10 +28,39 @@ builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
     .AddRoles<IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>();
 
-builder.Services.AddControllersWithViews();
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+// Метод миграции
+async Task MigrateAsync(IServiceProvider serviceProvider)
+{
+    using var scope = serviceProvider.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    var originalUsers = dbContext.Users.ToList();
+
+    foreach (var user in originalUsers)
+    {
+        var appUser = new ApplicationUser
+        {
+            UserName = user.Username,
+            Email = user.Email,
+            PhoneNumber = user.Phone,
+            LegacyUserId = user.UserId,
+        };
+
+        var result = await userManager.CreateAsync(appUser, user.Password); // Установите пароль по умолчанию или используйте существующий
+        if (!result.Succeeded)
+        {
+            throw new Exception($"Unable to create user {user.Username}: {result.Errors.First().Description}");
+        }
+    }
+}
+
+// Вызов метода миграции при запуске приложения
+await MigrateAsync(app.Services);
 
 if (!app.Environment.IsDevelopment())
 {
@@ -51,4 +82,4 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-app.Run();
+await app.RunAsync();
